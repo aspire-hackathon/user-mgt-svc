a41bdec57c3f8f4833cda279b84dc815ebf49ef9
Brajesh Anokha <anokha777@gmail.com>
2022-03-26 11:19:51 +0530

    db url

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   // mongoURI: 'mongodb://mongo:27017/user-db', 
-  mongoURI: 'mongodb+srv://avyaja:Avyaja_123@cluster0.cfdqn.mongodb.net/notification?retryWrites=true&w=majority',
+  // mongoURI: 'mongodb+srv://avyaja:Avyaja_123@cluster0.cfdqn.mongodb.net/notification?retryWrites=true&w=majority',
+  mongoURI: 'mongodb+srv://aspire:hackathon@cluster0.cfdqn.mongodb.net/usermgt?retryWrites=true&w=majority',
   slat: 'This is salt',
   secret: 'This_is_jwt_secret',
 };
