6a97551f0ab4dce3d1afa3b8c84fd192e11eedbb
Brajesh Anokha <anokha777@gmail.com>
2022-03-26 10:56:14 +0530

    db model

---
+++ src/models/CauseModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CauseSchema = new Schema({
+  title: { type: String, require: true },
+  aimDescription: { type: String, required : true },
+  causeImage: { type: String, required : true },
+  district: { type: String, required : true },
+  state: { type: String, required : true },
+  pincode: { type: String, required : true },
+  lat: { type: String, required : true },
+  lng: { type: String, required : true },
+  causeOwner: {type: Schema.Types.ObjectId, ref: 'user'},
+  volunteers: {type: [Schema.Types.ObjectId], ref: 'user'},
+  causeStatus: { type: String, required :false },
+  createdAt: { type: Date, default: Date.now() }
+});
+
+module.exports = mongoose.model('cause',CauseSchema);
--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -2,11 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  fname: { type: String, require: true },
-  lname: { type: String, require: true },
+  name: { type: String, require: true },
   username: { type: String, required : true },
   password: { type: String, required : true },
-  badge: { type: String, default: 'NA' },
+  role: { type: String, required : true },
+  contactNo: { type: String, required : true },
+  email: { type: String, required : true },
+  district: { type: String, required : true },
+  state: { type: String, required : true },
+  pincode: { type: String, required : true },
+  lat: { type: String, required : true },
+  lng: { type: String, required : true },
   createdAt: { type: Date, default: Date.now() }
 });
     
